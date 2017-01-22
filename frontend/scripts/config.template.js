9ba3508614ea7135c297726be22bb1421ad2ea73
Vikram Tiwari <vikramtheone1@gmail.com>
2017-01-21 16:56:23 -0800

    update with friendly chat

---
+++ frontend/scripts/config.template.js
@@ -0,0 +1,8 @@
+// NOTE: REPLACE WITH YOUR CREDENTIALS
+var config = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  databaseURL: "DATABASE_URL",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID"
+};
